d2a993b6a59f1442cf5852788d61fa94e14c6591
Pradumn-Singh-Patel <pradumnpatel18@gmail.com>
2024-01-02 23:38:43 +0530

    created room routes

---
+++ server/routes/rooms.js
@@ -0,0 +1,34 @@
+import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+  updateRoomAvailability,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    res.send('Room page')
+})
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//UPDATE
+router.put("/availability/:id", updateRoomAvailability);
+router.put("/:id", verifyAdmin, updateRoom);
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+//GET
+
+router.get("/:id", getRoom);
+//GET ALL
+
+router.get("/", getRooms);
+
+export default router;
+
+
