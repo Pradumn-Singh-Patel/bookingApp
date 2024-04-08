6808cee8e86fc65a8817fe3947fce83104d6c027
Pradumn-Singh-Patel <pradumnpatel18@gmail.com>
2024-04-09 01:19:52 +0530

    configured deployment

--- client/src/components/footer/FooterList.jsx
+++ client/src/components/footer/FooterList.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function FooterList({ keywords }) {
   return (
     <ul className="fList">
-      {keywords.map((item) => (
+      {keywords?.map((item) => (
         <li className="fListItem">{item}</li>
       ))}
     </ul>
